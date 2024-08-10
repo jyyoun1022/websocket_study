1f5ee8cb10e8bec21b179f77d705b34c8e7c8e31
jyyoun1022 <jyyoun1022@naver.com>
2024-08-10 23:43:53 +0900

    redis설정

---
+++ src/main/java/websocket/lab/config/redis/RedisConfig.java
@@ -0,0 +1,42 @@
+package websocket.lab.config.redis;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    // Redis Channel(Topic)으로 부터 메시지를 받고, 주입된 리스터에게 비동기적으로 dispatch하는 컨테이너
+    // 발행자와 구독자 간의 메시지 전달을 위한 논리적 채널
+    @Bean
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory redisConnectionFactory,
+                                                                       MessageListenerAdapter listenerAdapter,
+                                                                       ChannelTopic channelTopic) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+        container.addMessageListener(listenerAdapter, channelTopic);
+        return container;
+    }
+
+    // Redis 서버에는 bytes 코드만이 저장되므로 key와 value에 Serializer를 설정
+    @Bean
+    public RedisTemplate<String, Object> myRedis (RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
+    }
+    //  Topic 공유를 위해 Channel Topic을 빈으로 등록해 단일화 시켜준다.
+    @Bean
+    public ChannelTopic channelTopic() {
+        return new ChannelTopic("chatroom");
+    }
+}
