7431078b4645b2190b8b0e2f8337808430c024a7
jyyoun <jyyoun@aistudio.co.kr>
2024-08-30 13:11:33 +0900

    socket config 수정

--- src/main/java/websocket/lab/config/socket/WebSocketConfig.java
+++ src/main/java/websocket/lab/config/socket/WebSocketConfig.java
@@ -18,19 +18,25 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     // 메시지를 중간에서 라우팅 할 떄 사용하는 메시지 브로커를 구성
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // 해당 주소를 구독하는 클라이언트에게 메시지를 보낸다. (구독 요청의 prefix)
-        // 클라이언트 1번 채널을 구독하고자 할 때는 /sub/1과 같은 규칙을 따라야
-        registry.enableSimpleBroker("/sub");
-        // 메시지 발행 요청의 prefix,
-        // pub으로 시작하는 메시지만 해당 Broker에서 처리한다.
-        registry.setApplicationDestinationPrefixes("/pub");
+        /**
+         * 메시지 브로커의 설정을 구성함
+         * - 클라이언트와 서버 간의 메시지를 라우팅하고 브로드캐스트하는 역할
+         *
+         * 1. /topic : 클라이언트가 구독할 수 있는 경로를 설정
+         */
+        registry.enableSimpleBroker("/topic");
+        // client가 구독할 수 있는 경로 설정
+        // 이 경로로 시작하는 모든 메시지는 메시지 브로커에 의해 처리되고, 해당 경로를 구독한 클라이언트들에게 전송된다.
+
+        registry.setApplicationDestinationPrefixes("/test");
+        // client가 서버에 보내는 메시지의 경로를 설정
+        // 이 경로로 시작하는 메시지를 서버로 전송하면 이 메시지는 @MessageMapping 과 같은 컨트롤러 메서드에서 처리된다.
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // ex ) ws://localhost:9000/stomp/chat
         // 클라이언트에서 Websocket에 접속할 수 있는 endPoint를 지정
-        registry.addEndpoint("/stomp/chat")
+        registry.addEndpoint("/socketTest")
                 .setAllowedOriginPatterns("*").withSockJS();
     }
 
